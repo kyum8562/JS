8225e5c78981ab4022a20930a5a615a90753c628
김창겸 <kyum8562@naver.com>
2023-02-23 00:13:04 +0900

    0222 algo done

--- 푸드파이터대회.js
+++ 푸드파이터대회.js
@@ -1,4 +1,16 @@
 function solution(food) {
   let answer = "";
+  for (let i = 1; i < food.length; i++) {
+    food[i] = parseInt(food[i] / 2);
+    if (food[i] >= 1) answer += `${i}`.repeat(food[i]);
+  }
+  answer += "0";
+  for (let i = food.length; i >= 1; i--) {
+    if (food[i] >= 1) answer += `${i}`.repeat(food[i]);
+  }
+
   return answer;
 }
+
+console.log(solution([1, 3, 4, 6]));
+console.log(solution([1, 7, 1, 2]));
