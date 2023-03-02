63b2e0a970b396b791c142415fb8d4423507a70e
김창겸 <kyum8562@naver.com>
2023-03-02 23:31:32 +0900

    0302 algo done

--- 숫자의표현.js
+++ 숫자의표현.js
@@ -2,10 +2,9 @@ function solution(n) {
   let answer = 0;
 
   for (let i = 0; i <= n; i++) {
-    if (n % i === 0 && i % 2 === 1)  answer++;
+    if (n % i === 0 && i % 2 === 1) answer++;
   }
 
   return answer;
 }
-console.log(solution(15));
 console.log(solution(15));
